6b278408a8bd74eb816c26bd85ab8a16eb515a63
Jeff Muthoki <muthokijeff13@gmail.com>
2022-09-14 11:59:51 -0700

    Task number 13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "main.h"
 /**
  * main - Entry point
- *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
+	int i, res;
+
+	for (i = 0; i < 1024; i++)
+		if (i % 3 == 0 || i % 5 == 0)
+			res += i;
+	printf("%d\n", res);
 	return (0);
 }
