e7abc158ef05ecba3d7223daddfa28cdc06a44fd
Jeff Muthoki <muthokijeff13@gmail.com>
2022-09-14 08:55:41 -0700

    Task number 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,14 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
  * main - prints the string "_putchar" from a character array.
+ *
  * Return: 0 on success.
  */
 int main(void)
 {
-	char c[] = "Holberton";
-	int i;
-
-	for (i = 0; i < 9; i++)
-	{
-		_putchar(c[i]);
-	}
-	_putchar('\n');
+	printf("_putchar\n");
+	
 	return (0);
 }
