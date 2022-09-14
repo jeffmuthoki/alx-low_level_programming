a598fb341b0a49d248747b62ad6facd733691bb5
Jeff Muthoki <muthokijeff13@gmail.com>
2022-09-14 09:07:56 -0700

    Task number 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabets
+ *
+ * Return: Success
+ */
+void print_alphabet(void)
+{
+
+	int n;
+
+	for (n = 'a'; n,= 'z'; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
