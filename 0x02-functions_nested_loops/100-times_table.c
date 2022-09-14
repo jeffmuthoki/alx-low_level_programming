03c7872ad96e15555f4c9d20d5cddb2dc3ea9442
Jeff Muthoki <muthokijeff13@gmail.com>
2022-09-14 11:55:51 -0700

    Task number 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -20,7 +20,7 @@ void print_times_table(int n)
 					_putchar(',');
 					_putchar(' ');
 				}
-				if (res < 10 && j !=0)
+				if (res < 10 && j != 0)
 				{
 					_putchar(' ');
 					_putchar(' ');
@@ -36,7 +36,7 @@ void print_times_table(int n)
 				{
 					_putchar((res / 100) + '0');
 					_putchar((res / 10) % 10 + '0');
-					_putchar((res % 10) +'0');
+					_putchar((res % 10) + '0');
 				}
 				else
 				{
