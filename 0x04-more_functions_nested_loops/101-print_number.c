045252e09d96a0656929a908719d00e9841d763c
Jeff Muthoki <muthokijeff13@gmail.com>
2022-09-16 04:52:39 -0700

    Task number 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,23 +9,24 @@ void print_number(int n)
 
 	res = n;
 	expo = len =  1;
-
+/*Check the negatives*/
 	if (res < 0)
 	{
 		res *= -1;
 		_putchar('-');
 	}
 
+/**/	
 	temp = res;
 	while (temp >= 10)
 	{
 		len++;
 		temp /= 10;
 	}
-
+/*Create Exponent*/
 	for (i = 1; i < len; i++)
 		expo *= 10;
-
+/*Main*/
 	while (expo > 1)
 	{
 		_putchar((res / expo) % 10 + '0');
