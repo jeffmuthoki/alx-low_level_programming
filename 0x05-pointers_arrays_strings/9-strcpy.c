44aef7c5bd781111e820c7c426d8f9ba2089eb84
Jeff Muthoki <muthokijeff13@gmail.com>
2022-09-19 11:53:31 -0700

    Task number 10

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,11 +8,13 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
+	int index = 0;
 
-	for (; src[i] != '\0'; i++)
+	while (src[index])
 	{
-		dest[i] = src[i];
+		dest[index] = src[index];
+		index++;
 	}
+
 	return (dest);
 }
