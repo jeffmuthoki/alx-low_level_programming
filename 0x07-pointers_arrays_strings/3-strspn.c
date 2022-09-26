61076bf7c4b2847e8380f882c128bb602d2ecbd0
Jeff Muthoki <muthokijeff13@gmail.com>
2022-09-26 11:41:22 -0700

    Task number 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,22 +9,25 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-int count;
+	unsigned int i, n, value, check;
 
-while (*s != '\0')
-{
-	while (*accept != '\0')
+	value = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (*s == *accept)
-			break;
-			count++;
-			accept++;
+		check = 0;
 
-			s++;
+		for (n = 0; accept[n] != '\0'; n++)
+		{
+			if (accept[n] == s[i])
+			{
+				value++;
+				check = 1;
+			}
 		}
-if (*accept == '\0')
-	break;
-		
-}
-	return (count + 1);
+
+		if (check == 0)
+			return (value);
+	}
+	return (value);
 }
