cf9de3fb2d648cb0a03369091b93da7123785c88
Jeff Muthoki <muthokijeff13@gmail.com>
2022-10-15 12:13:13 -0700

    Task number 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,51 +1,77 @@
 #include "variadic_functions.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * print_all - prints anything
- * @format: list of types or arguments to be passed
- * Return: void
+ * _printchar - print char type element from va_list
+ * @list: va_list passed to function
  */
 
+void _printchar(va_list list)
+{
+	printf("%c", va_arg(list, int));
+}
+/**
+ * _printstr - print string element from va_list
+ * @list: va_list passed to function
+ */
+void _printstr(va_list list)
+{
+	char *s;
+
+	s = va_arg(list, char *);
+	if (s == NULL)
+		s = "(nil)";
+	printf("%s", s);
+}
+/**
+ * _printfloat - print float type element from va_list
+ * @list: va_list passed to function
+ */
+void _printfloat(va_list list)
+{
+	printf("%f", va_arg(list, double));
+}
+
+/**
+ * _printint - print int type element from va_list
+ * @list: va_list passed to function
+ */
+void _printint(va_list list)
+{
+	printf("%d", va_arg(list, int));
+}
+/**
+ * print_all - print anything passed if char, int, float, or string.
+ * @format: string of formats to use and print
+ */
 void print_all(const char * const format, ...)
 {
-	va_list valist;
-	char *temp_s;
-	int i = 0, orders;
+	unsigned int i, j;
+	va_list args;
+	char *sep;
 
-	while (format && format[i])
+	checker storage[] = {
+		{ "c", _printchar },
+		{ "f", _printfloat },
+		{ "s", _printstr },
+		{ "i", _printint }
+	};
+
+	i = 0;
+	sep = "";
+	va_start(args, format);
+	while (format != NULL && format[i / 4] != '\0')
 	{
-		va_start(valist, format);
-		while (format[i])
+		j = i % 4;
+		if (storage[j].type[0] == format[i / 4])
 		{
-			orders = 1;
-			switch (format[i++])
-			{
-			case 'c':
-				printf("%c", va_arg(valist, int));
-				break;
-			case 'i':
-				printf("%d", va_arg(valist, int));
-				break;
-			case 'f':
-				printf("%f", va_arg(valist, double));
-				break;
-			case 's':
-				temp_s = va_arg(valist, char*);
-				if (temp_s)
-				{
-					printf("%s", temp_s);
-					break;
-				}
-				printf("(nil)");
-				break;
-			default:
-				orders = 0;
-				break;
-			}
-			if (format[i] && orders)
-				printf(", ");
+			printf("%s", sep);
+			storage[j].f(args);
+			sep = ", ";
 		}
-		va_end(valist);
+		i++;
 	}
 	printf("\n");
+	va_end(args);
 }
